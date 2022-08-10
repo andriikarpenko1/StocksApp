46f8f72b7dc750b487e2a02738acde0598217a56
Andrey Karpenko <ximerg002@gmail.com>
2022-08-10 22:34:10 +0400

    Initial Commit

---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by Andrey Karpenko  on 10.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
